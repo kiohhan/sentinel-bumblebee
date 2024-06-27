4028d5f346f4dc6c3e2e3b0ebf6dfe86900efd1c
Kioh <kiohhan.dev@gmail.com>
2024-06-27 19:19:23 +0000

    yello side nav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,17 +5,19 @@ import Image from 'next/image';
 export default function SideNav() {
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
-            <Link
-                className="mb-2 flex h-20 items-end justify-start rounded-md bg-yellow-300 p-4 md:h-40"
-                href="/"
-            >
-                <Image
-                    src="/images/bb_icon.svg"
-                    alt="bumblebee"
-                    width={100}
-                    height={100}
-                />
-            </Link>
+            <div className="flex flex-row bg-yellow-300 p-4">
+                <Link
+                    href="/"
+                >
+                    <Image
+                        src="/images/bb_icon.svg"
+                        alt="bumblebee"
+                        width={100}
+                        height={100}
+                    />
+                </Link>
+            </div>
+
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <NavLinks />
                 <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
