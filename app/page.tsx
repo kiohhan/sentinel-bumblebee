ed021b7d80112db979f04d73fab74b8ad8acbf54
Kioh <kiohhan.dev@gmail.com>
2024-06-27 18:22:26 +0000

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import styles from '@/app/ui/styles/styles.module.css'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-blue-500">I'm blue!</h1>
+      <h1 className="text-blue-500">Bumblebee</h1>
       <div
         className={styles.shape}
       />
+      <div>
+        <Image
+          src="/images/bb_icon.svg"
+          alt="bumblebee"
+          width={300}
+          height={300}
+        />
+      </div>
     </main>
   );
 }
