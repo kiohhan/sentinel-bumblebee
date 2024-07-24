9a1701f4b22662d79338970659364c7f7d9867d9
Kioh <kiohhan.dev@gmail.com>
2024-07-24 19:43:52 +0000

    add shadcn

--- app/ui/object/create-form.tsx
+++ app/ui/object/create-form.tsx
@@ -1,24 +1,24 @@
 import { createObject } from "@/app/lib/actions/object/actions"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function CreateObjectForm() {
-    return <form action={createObject}>
+    return <form className="mt-3" action={createObject}>
         <div className="flex-col grid gap-4">
             {/* object name */}
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-4">
-                    <label className="block text-sm font-medium leading-6 text-gray-900">Object Name</label>
-                    <div>
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                            <input type="text" name="name" id="name" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="my_object" />
-                        </div>
-                    </div>
-                </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="name">Object Name</Label>
+                <Input type="text" name="name" id="name" placeholder="obj1" />
             </div>
 
             {/* fields */}
             {/* workflow */}
 
         </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Create Object</button>
+        <div className="flex mt-3">
+            <Button type="submit">Create Object</Button>
+        </div>
+
     </form>
 }
