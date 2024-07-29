9d494228fe98ac20b7e4df4b8261959939181056
Kioh <kiohhan.dev@gmail.com>
2024-07-29 19:19:55 +0000

    remove timeout

--- app/lib/db/select.ts
+++ app/lib/db/select.ts
@@ -5,7 +5,7 @@ export const findManyRaw = async (table: string) => {
     try {
         const query = `SELECT * FROM ${table}`
         const res = await myPool.query(query)
-        await new Promise(r => setTimeout(r, 2000));
+        // await new Promise(r => setTimeout(r, 2000));
         return res.rows
     } catch (e) {
         throw e
@@ -16,7 +16,7 @@ export const findOneRaw = async (table: string, id: string) => {
     try {
         const query = `SELECT * FROM ${table} WHERE id=${id}`
         const res = await myPool.query(query)
-        await new Promise(r => setTimeout(r, 2000));
+        // await new Promise(r => setTimeout(r, 2000));
         return res.rows[0]
     } catch (e) {
         throw e
